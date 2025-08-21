82ff2a8f265a315b6b86ba4774b39fb4cf4ccb02
Daniel <30896817+chondaen12@users.noreply.github.com>
2025-08-21 23:28:07 +0700

    Update signals.py

--- src/pretix/plugins/stripe/signals.py
+++ src/pretix/plugins/stripe/signals.py
@@ -19,8 +19,8 @@
 # You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
 # <https://www.gnu.org/licenses/>.
 #
-import json
 from collections import OrderedDict
+import json
 
 from django import forms
 from django.dispatch import receiver
